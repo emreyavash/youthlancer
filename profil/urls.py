5f63b35a412dac21ed979909b0d91a7fe3a0f75e
emreyavash <emre.yavas1@ogr.sakarya.edu.tr>
2022-03-18 17:35:08 +0300

    Profil ve Hesap Ayarları Template

--- profil/urls.py
+++ profil/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-
+from . import views
 urlpatterns = [
-    
+    path('profilim/<str:username>',views.profil_view,name="profil"),
+    path('hesap-ayarlari/<str:username>',views.hesapAyarlari_view,name="hesap-ayarlari")
 ]
--- profil/views.py
+++ profil/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def profil_view(request,username):
+
+    return render(request,'profil/profil.html')
+
+def hesapAyarlari_view(request,username):
+    
+    return render(request,'profil/hesapayarlari.html')
