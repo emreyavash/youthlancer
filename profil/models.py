0266ecbda0bf928aa0ceadbf1ecec40ed4e43c3d
emreyavash <emre.yavas1@ogr.sakarya.edu.tr>
2022-03-19 13:25:04 +0300

    hesap ayarlari ve Profil

--- profil/models.py
+++ profil/models.py
@@ -17,8 +17,9 @@ class Universite(models.Model):
 class Kullanici(models.Model):
     user=models.ForeignKey(User,on_delete=models.DO_NOTHING,null=False,blank=True)
     kullanici_profil=models.ImageField(upload_to='kullanici',null=True)
-    protfoy = models.ImageField(upload_to="portföy",null=True )
+    dogum_gunu = models.DateField(null=True)
     freelancer = models.BooleanField(default=False)
+    telefon = models.IntegerField(null=True)
     isveren = models.BooleanField(default=False)
     sehir = models.ForeignKey(Sehir,on_delete=models.SET_NULL,null=True)
     universite = models.ForeignKey(Universite,on_delete=models.SET_NULL,null=True)
@@ -29,3 +30,6 @@ class Kullanici(models.Model):
     def save(self,*args, **kwargs):
         self.slug = slugify(self.user.first_name,self.user.last_name)
         super().save(*args, **kwargs)
+
+class Yetenekler(models.Model):
+    pass
--- profil/views.py
+++ profil/views.py
@@ -1,11 +1,74 @@
-from django.shortcuts import render
-
+from multiprocessing import context
+from django.shortcuts import redirect, render
+from .models import Kullanici,Sehir,Universite
+from django.contrib.auth.models import User
 # Create your views here.
 
 def profil_view(request,username):
-
-    return render(request,'profil/profil.html')
+    user = User.objects.get(username = username)
+    kullanici = Kullanici.objects.filter(user = user)
+    if kullanici.exists():
+        kullanici = Kullanici.objects.get(user = user)
+        context={
+            'kullanici':kullanici
+        }
+        return render(request,'profil/profil.html',context)
+    else:
+        return hesapAyarlari_view(request,username)
 
 def hesapAyarlari_view(request,username):
-    
-    return render(request,'profil/hesapayarlari.html')
+    universite = Universite.objects.all()
+    sehir = Sehir.objects.all()
+    user = User.objects.get(username=username)
+    kullanici = Kullanici.objects.filter(user=user)
+
+    if request.method == "POST":
+        
+        if kullanici.exists():
+            kullanici_bilgileri = Kullanici.objects.get(user=user)
+            kullanici_bilgileri.kullanici_profil = request.FILES.get("profil-foto",False)
+            kullanici_bilgileri.aciklama = request.POST["aciklama"]
+            kullanici_bilgileri.dogum_gunu = request.POST['dogum_gunu']
+            sehir_id = request.POST['sehir']
+            universite_id = request.POST['universite']
+            kullanici_bilgileri.sehir =Sehir.objects.get(id=sehir_id)
+            kullanici_bilgileri.universite = Universite.objects.get(id = universite_id)
+            kullanici_bilgileri.save()
+            context={
+                'sehirler':sehir,
+                'universite':universite,
+                'kullanici':kullanici_bilgileri
+            }
+            return render(request,'profil/hesapayarlari.html',context)
+        else:
+            profil_foto = request.FILES.get("profil-foto",False)
+            aciklama = request.POST["aciklama"]
+            sehir = request.POST['sehir']
+            universite = request.POST['universite']
+            dogum_gunu = request.POST["dogum_gunu"]
+            sehir_id =Sehir.objects.get(id=sehir)
+            universite_id = Universite.objects.get(id = universite)
+
+            kullanici = Kullanici.objects.create(kullanici_profil = profil_foto,aciklama=aciklama,sehir=sehir_id,universite=universite_id,user=user,dogum_gunu =dogum_gunu)
+            kullanici.save()
+        
+            return redirect('hesap-ayarlari',username)
+    else:
+        if kullanici.exists():
+            kullanici_bilgileri = Kullanici.objects.get(user=user)
+
+            context={
+                'sehirler':sehir,
+                'universite':universite,
+                'kullanici':kullanici_bilgileri
+            }
+            return render(request,'profil/hesapayarlari.html',context)
+        else:
+            context={
+                'sehirler':sehir,
+                'universite':universite,
+                
+                'error':'Profil Bilgilerini Doğrulamadan Profiliniz Gözükmez.'
+            }
+            return render(request,'profil/hesapayarlari.html',context)
+
